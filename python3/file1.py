0700607747f58047a2a5a7e0091b63869c4c0b56
? <Andresicokk@gmail.com>
2025-01-06 20:58:53 -0500

    FirstExercise

--- python3/file1.py
+++ python3/file1.py
@@ -1,1 +1,12 @@
-print('guacamalla')
+#https://leetcode.com/problems/two-sum/description/
+
+def sum(array: list, target: int) -> int:
+    for i in  array:
+        for f in array:
+            if (i +f) == target:
+             print(i,f)
+
+    
+        
+    
+
