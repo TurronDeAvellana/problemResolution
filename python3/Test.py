d3d9d28ec540455a1b7e01f155d33ce30474b14c
? <Andresicokk@gmail.com>
2025-01-19 19:20:48 -0500

    parenthesys

--- python3/Test.py
+++ python3/Test.py
@@ -1,9 +1,12 @@
 from file1 import *
 
-sum([1,2,3,4,5], 10)
+#sum([1,2,3,4,5], 10)
 
-isPalindrome(121)
+#isPalindrome(121)
 
+validParenthesis(['(',')'])
+validParenthesis(['(',']'])
+validParenthesis(['(',')','{'])
 
 
             
--- python3/file1.py
+++ python3/file1.py
@@ -6,6 +6,8 @@ def sum(array: list, target: int) -> int:
             if (i +f) == target:
              print(i,f)
 
+#--------------------------------------------------------------------------------------------                   
+
 # https://leetcode.com/problems/palindrome-number/description/
 
 def isPalindrome (n: int) -> bool:
@@ -19,5 +21,32 @@ def isPalindrome (n: int) -> bool:
         print(False)
 
     
+#--------------------------------------------------------------------------------------------      
+# https://leetcode.com/problems/valid-parentheses/description/
 
-   
+#This funcion does nort verify parenthesys order, only if the number of open and close parenthesis is the same
+
+def validParenthesis (a: list) -> bool:
+
+    c1, c2 = 0, 0  # ()
+    c3, c4 = 0, 0  # {}
+    c5, c6 = 0, 0  # []
+     
+    for i in a:
+        if (i == '('):
+            c1 += 1
+        elif (i == ')'):
+            c2 += 1
+        elif (i == '{'):
+            c3 += 1
+        elif (i == '}'):
+            c4 += 1
+        elif (i == '['):
+            c5 += 1
+        elif (i == ']'):
+            c6 += 1    
+
+    if (c1 == c2 ) and (c3 == c4 ) and (c5 == c6):
+        print("True")
+    else:
+        print("False")
